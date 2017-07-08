8cfa00f76760aa7182fca0035e65e97e057cd939
Kryeger <neagudavid@gmail.com>
2017-07-09 02:56:31 +0300

    snap
    - progress on objects

--- js/func.js
+++ js/func.js
@@ -1,6 +1,16 @@
 (function (window, $) {
 	
 
+	//methods
+	
+	function inheritPrototype(childObject, parentObject){
+		var copyOfParent = Object.create(parentObject.prototype);
+		copyOfParent.constructor = childObject;
+		childObject.prototype = copyOfParent;
+	}
+	
+	//obj
+	
 	function User(name){
 		this.name = name;
 		this.money = 1000;
@@ -17,7 +27,9 @@
 	function Company(name, capital){
 		this.name = name;
 		this.capital = capital;
+		this.workers = [];
 		this.currentProjects = [];
+		this.jobsQueue = [];
 		this.releasedGames = [];
 	}
 	
@@ -25,9 +37,49 @@
 		constructor: Company,
 		developNewGame: function(){
 			this.currentProjects.push(new Game("name1"));
+		},
+		createNewJob: function(){
+			this.jobsQueue.push(new Project("name1", "game1", 100));
 		}
 	}
+	
+	function Worker(specialization, skill, age){
+		this.name = chance.first + " " + chance.last;
+		this.specialization = specialization;
+		this.skill = skill;
+		this.previousProjects = [];
+		this.age = age;
+		this.currentJob = "";
+		this.avatar = "";
 		
+		this.level = 1;
+		this.exp = 0;
+		this.toNextLevel = 10;
+	}
+		
+	Worker.prototype = {
+		constructor: Worker,
+		hire: function(){
+			//hire
+		},
+		fire: function(){
+			//fire
+		},
+		promote: function(){
+			//promote
+		},
+		levelUp: function(){
+			this.level ++;
+			this.exp = 0;
+		},
+		checkForJobs: function(jobsQueue){
+			if(this.currentJob == "")
+				jobsQueue.forEach(function(job){
+					
+			});
+		}
+	}
+	
 	function Game(name){
 		this.name = name;
 	}
@@ -39,8 +91,25 @@
 		}
 	}
 	
-	$(document).ready(function(){
+	function Job(name, type, duration){
+		this.name = name;
+		this.type = type;
+		this.duration = duration;
+		this.progress = 0;
+	}
 	
+	Job.prototype = {
+		constructor: Job,
+		progress: function(amount){
+			this.progress += amount;
+			if(this.progress >= this.duration){
+				//finish method
+			}
+		}
+	}
+	
+	$(document).ready(function(){
+		
 		var worldTick = setInterval(function(){
 			
 		}, 250);
