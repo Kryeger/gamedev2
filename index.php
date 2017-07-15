7b72d88e8062804edb693dc05aefb4357ec83fb8
ShadyART <mariusgeorgem@yahoo.com>
2017-07-16 01:13:53 +0300

    snap
    - added details about the main company in the top-bar;
    - added possibility to view a list of the companies you own;
    - added possibility to create a new company;
    - added possibility to Add or Withdraw funds from your Main Company;
    - other minor tweaks;

--- index.php
+++ index.php
@@ -18,7 +18,10 @@ include_once("php/func.php");
 		<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,500italic,400italic,300italic' rel='stylesheet' type='text/css'>
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet">
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jStorage/0.4.12/jstorage.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/2.5.0/math.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/taffydb/2.7.2/taffy-min.js"></script>
@@ -33,32 +36,129 @@ include_once("php/func.php");
 
     <body>
         
-        <div class="modalOverlay">
-            <div class="modalCustomCharacter modalWindow">
-                <div class="mcc-top">
-                    <div class="mcc-top-title">Character Customization</div>
-                    <div class="mcc-top-close closeModalOv"><i class="material-icons">close</i></div>
+        
+        <!-- 
+        <div class="trs-fundsWrap">
+            <div class="trs-fundsWrapTop">
+                <div class="trs-fwt-name">Capital</div>
+                <div class="trs-fwt-tabs">
+                    <div class="trs-fwt-tab selected" data-tab="0">Add funds</div>
+                    <div class="trs-fwt-tab" data-tab="1">Withdraw</div>
                 </div>
-                <div class="mcc-cont">
-                    <div class="mcc-picture" style="background-image: url(php/face.php);"></div>
-                    <div class="mcc-options">
-                        <div class="mcc-option-cont">
-                            <div class="mcc-option-main">Facial Hair <i class="material-icons">add</i></div>
-                            <div class="mcc-option">
-                                <span>Facial Hair #1</span>
-                                <div class="mcc-option-buy"></div>
-                            </div>
-                        </div>
+            </div>
+            <div class="trs-fundsCont">
+                <div class="trs-funds-sign">+</div>
+                <label>$</label>
+                <input class="trs-funds-input">
+            </div>
+            <div class="trs-fundsWarning">
+                Insufficient funds
+            </div>
+            <div class="trs-funds-buttonWrap">
+                <div class="trs-funds-button">Add</div>
+            </div>
+        </div>
+        -->
+        <!--
+        <div class="modalOverlay">
+            <div class="founding-contract">
+                <div class="founding-contract-title">Bill of Property</div>
+                <div class="fc-topDetails">
+                    <div class="fc-topDetail">
+                        <div class="fc-td-left">Name</div>
+                        <div class="fc-td-right username"></div>
+                    </div>
+                    <div class="fc-topDetail">
+                        <div class="fc-td-left">Company Name</div>
+                        <input class="fc-td-right username" type="text" name="compName">
+                    </div>
+                    <div class="fc-topDetail">
+                        <div class="fc-td-left">Capital</div>
+                        <label>$</label>
+                        <input class="fc-td-right username" type="number" name="compCap">
+                    </div>
+                </div>
+                <div class="fc-cont">
+                    Capicola pork shoulder drumstick, pork turducken tip kielbasa. Mignon ham ham 
+                    belly biltong, hamburger round ham tri-tip braunschweiger. Drumstick strip 
+                    leberkas landjaeger tip beef braunschweiger tenderloin pork sausage, pork 
+                    sirloin frankfurter ham turkey ham ham boudin brisket. Turducken jerky bacon ham 
+                    pork. Ribs turducken fatback, strip prosciutto turducken ham shankle ham chuck 
+                    andouille
+                </div>
+                <div class="fc-bottom">
+                    <div class="fc-bottom-date">
+                        <div class="fc-bottom-date-title">Date</div>
+                        <div class="fc-bottom-date-date worldFormatedDate"></div>
+                    </div>
+                    
+                    <div class="fc-bottom-signature">
+                        <div class="fc-bottom-sign-title">Signature</div>
+                        <div class="fc-bottom-sign-button-cancel">Cancel</div>
+                        <div class="fc-bottom-sign-button">Sign here</div>
                     </div>
                 </div>
             </div>
         </div>
+        -->
         
         <div class="topLevel">
             <div class="top-leftside">
                 <div class="topLogo">gamedev<span class="version">0.0</span></div>
             </div>
             <div class="top-rightside clearfix">
+                <div class="trs-companyWrap">
+                    <div class="trs-companyAvt" style="background-image: url(http://i.imgur.com/Tz8ZXAe.png);"></div>
+                    <div class="trs-mainComp noSelect">
+                    <div class="trs-companyName textScrollWrap">
+                        <div class="mainCompanyName"></div>
+                    </div>
+                        <i class="material-icons trs-toggleCompaniesList">arrow_drop_down</i>
+                    </div>
+                    <div class="trs-companyItem trs-openFunds">
+                        <i class="material-icons">account_balance</i>
+                        $<span class="mainCompanyCapital"></span>
+                    </div>
+
+                    <div class="trs-companyItem">
+                        <i class="material-icons">people</i>
+                        <span class="mainCompanyWorkersNum"></span>
+                    </div>
+                    
+                    <div class="trs-companyItem">
+                        <i class="material-icons">burst_mode</i>
+                        <span class="mainCompanyReleasedGamesNum"></span>
+                    </div>
+
+                    <div class="trs-companyItem">
+                        <i class="material-icons">face</i>
+                        <span class="mainCompanyFansNum"></span>
+                    </div>
+                    
+                    <!-- 
+                    <div class="trs-allComps">
+                        <div class="trs-allCompsTop"> Your companies </div>
+                        <div class="trs-allCompsList">
+                            <div class="trs-allCompsItem">
+                                <div class="trs-aci-avt" style="background-image: url(http://i.imgur.com/Tz8ZXAe.png);"></div>
+                                <div class="trs-aci-info">
+                                    <div class="trs-aci-name textScrollWrap"><div class="mainCompanyName"></div>
+                                    </div>
+                                    <div class="trs-aci-details clearfix">
+                                        <div class="trs-aci-detail mainCompanyCapital">Capital: $999</div>
+                                        <div class="trs-aci-detail mainCompanyWorkersNum">Workers: 10</div>
+                                        <div class="trs-aci-detail mainCompanyReleasedGamesNum">Released Games: 2</div>
+                                        <div class="trs-aci-detail mainCompanyFansNum">Fans: 2</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="trs-addNewComp">
+                            <div class="trs-addNewCompButton">Found New Company</div>
+                        </div>
+                    </div>
+                    -->
+                </div>
                 <div class="trs-money">$<span class="usermoney"></span></div>
                 <div class="trs-name username"></div>
                 <div class="trs-avt useravatar"></div>
--- js/ui.js
+++ js/ui.js
@@ -16,7 +16,23 @@
             return ret;
         }
     }
+    
+    
+    function refreshInfo(){
+        $(".username").text(user.name);
+        $(".usermoney").text(numberWithCommas(user.money));
+        $(".useravatar").css("background-image", "url("+ getAvt(user) +")");
+        $(".version").text(VERSION);
+        $(".ls-mi-title").text(user.mainCompany.name);
+        $(".mainCompanyName").text(user.mainCompany.name);
+        $(".mainCompanyCapital").text(numberWithCommas(user.mainCompany.capital));
+        $(".mainCompanyWorkersNum").text(numberWithCommas(Object.keys(user.mainCompany.workers).length));
+        $(".mainCompanyReleasedGamesNum").text(numberWithCommas(Object.keys(user.mainCompany.releasedGames).length + Object.keys(user.mainCompany.archive).length));
+        $(".mainCompanyFansNum").text(numberWithCommas(user.mainCompany.fans));
+        $(".worldFormatedDate").text(world.formatedDate);
+    }
 
+    
     $(document).ready(function(){
         
         // cookies
@@ -26,11 +42,7 @@
         }
         
         // init
-        $(".username").text(user.name);
-        $(".usermoney").text(numberWithCommas(user.money));
-        $(".useravatar").css("background-image", "url("+ getAvt(user) +")");
-        $(".version").text(VERSION);
-		$(".ls-mi-title").text(user.mainCompany.name);
+        refreshInfo();
 
         // interaction
         $(document).on("click", ".ls-toggler", function(){
@@ -44,8 +56,270 @@
             }
         });
         
-        $(document).on("click", ".closeModalOv", function(){
-            $(".modalOverlay").remove(); 
+        var textScroll;
+        
+        $(".textScrollWrap").hover(function(){
+            var move = 5;
+            var parent = $(this);
+            if(parent.outerWidth() <= parseInt($(this).find("div").width())){
+                console.log(parent.outerWidth());
+            textScroll = setInterval(function(){
+                var spanLeft = parseInt(parent.find("div").css("left"));
+                parent.find("div").css("left", (spanLeft - move) +"px");
+                if(-spanLeft >= parseInt(parent.find("div").width()) - parseInt(parent.width())) move = -5;
+                else if(-spanLeft <= 0) move = 5;
+            }, 100);
+            }
+        }, function(){
+            clearInterval(textScroll);
+            $(this).find("div").css("left", "0px");
+        });
+        
+        $(document).on("click", ".trs-mainComp", function(){
+            if(!$('.trs-allComps').length){
+           $(this).addClass("tgld"); 
+                var left = $(".trs-companyWrap").position().left;
+            $("body").prepend(`
+<div class="trs-allComps" style="left: `+left+`px;">
+<div class="trs-allCompsTop"> Your companies </div>
+<div class="trs-allCompsList">
+
+</div>
+<div class="trs-addNewComp">
+<div class="trs-addNewCompButton">Found New Company</div>
+</div>
+</div>
+`);
+                if(Object.keys(user.companies).length > 0){
+                    var compNum = Object.keys(user.companies).length;
+                    for(var i = 0; i < compNum; i++){
+                        $(".trs-allCompsList").append(`
+<div class="trs-allCompsItem">
+<div class="trs-aci-avt" style="background-image: url(http://i.imgur.com/Tz8ZXAe.png);"></div>
+<div class="trs-aci-info">
+<div class="trs-aci-name textScrollWrap"><div>`+user.companies[i].name+`</div>
+</div>
+<div class="trs-aci-details clearfix">
+<div class="trs-aci-detail">Capital: $`+(numberWithCommas(user.companies[i].capital))+`</div>
+<div class="trs-aci-detail">Workers: `+(numberWithCommas(Object.keys(user.companies[i].workers).length))+`</div>
+<div class="trs-aci-detail">Released Games: `+(numberWithCommas(Object.keys(user.companies[i].archive).length + Object.keys(user.companies[i].releasedGames).length))+`</div>
+<div class="trs-aci-detail">Fans: `+(numberWithCommas(user.companies[i].fans))+`</div>
+</div>
+</div>
+</div>
+`); 
+                    }
+                }
+            }
+        });
+        
+        $(document.body).on("click", ":not(.trs-allComps *, .trs-allComps)", function(e){
+            if($(".trs-allComps").length){
+                $(".trs-allComps").remove();
+                $(".trs-mainComp").removeClass("tgld");
+                e.stopPropagation();
+                return false;
+            }
+        });
+        
+        $(document).on("click", ".trs-fwt-tab", function(){
+           var tab = parseInt($(this).attr("data-tab"));
+            $(".trs-fwt-tab").removeClass("selected");
+            $(this).addClass("selected");
+            $(".trs-fundsWarning").remove();
+            if(tab == 0){ // add funds
+                $(".trs-funds-sign").text("+");
+                $(".trs-funds-sign").removeClass("red");
+                $(".trs-funds-button").text("Add");
+                $(".trs-funds-button").removeClass("red");
+            }else if(tab == 1){
+                $(".trs-funds-sign").text("-");
+                $(".trs-funds-sign").addClass("red");
+                $(".trs-funds-button").text("Withdraw");
+                $(".trs-funds-button").addClass("red");
+            }
+        });
+        
+        $(document).on("click", ".trs-openFunds", function(){
+           $(this).addClass("tgld");
+            var left = $(this).offset().left;
+            if(!$(".trs-fundsWrap").length){
+            $("body").prepend(`
+<div class="trs-fundsWrap" style="left: `+left+`px;">
+<div class="trs-fundsWrapTop">
+<div class="trs-fwt-name">Capital</div>
+<div class="trs-fwt-tabs">
+<div class="trs-fwt-tab selected" data-tab="0">Add funds</div>
+<div class="trs-fwt-tab" data-tab="1">Withdraw</div>
+</div>
+</div>
+
+<div class="trs-fundsCont">
+<div class="trs-funds-sign">+</div>
+<label>$</label>
+<input class="trs-funds-input" type="number">
+</div>
+
+<div class="trs-funds-buttonWrap">
+<div class="trs-funds-button">Add</div>
+</div>
+</div>
+`);
+            }
+        });
+        
+        $(document.body).on("click", ":not(.trs-fundsWrap, .trs-fundsWrap *)", function(e){
+           if($(".trs-fundsWrap").length){
+               e.stopPropagation();
+               $(".trs-fundsWrap").remove();
+               $(".trs-openFunds").removeClass("tgld");
+           } 
+        });
+        
+        $(document).on("click", ".trs-funds-button", function(){
+            var tab = parseInt($(".trs-fwt-tab.selected").attr("data-tab")); 
+            var canDo = 1;
+            var sum = parseFloat($(".trs-funds-input").val());
+            $(".trs-fundsWarning").remove();
+            if(!$.isNumeric(sum)){
+                $canDo = 0;
+                $(".trs-funds-buttonWrap").before(`
+<div class="trs-fundsWarning">
+You must enter a number
+</div>
+`);
+            }
+            if(canDo){
+                
+                if(tab == 0){ // add funds
+                    if(sum > user.money){
+                        $(".trs-funds-buttonWrap").before(`
+<div class="trs-fundsWarning">
+Insufficient funds
+</div>
+`);
+                    }else{
+                        user.money -= sum;
+                        user.mainCompany.capital += sum;
+                    }
+                }else{ // withdraw funds
+                    if(sum > user.mainCompany.capital){
+                        $(".trs-funds-buttonWrap").before(`
+<div class="trs-fundsWarning">
+Insufficient funds
+</div>
+`);
+                    }else{
+                        user.money += sum;
+                        user.mainCompany.capital -= sum;
+                    }
+                }
+                
+                refreshInfo();
+                
+            }
+        });
+        
+        $(document).on("click", ".fc-bottom-sign-button", function(){
+            var companyName = $("input[name=compName]").val();
+            var companyCap = parseFloat($("input[name=compCap]").val());
+            var canDo = 1;
+            
+            $(".fc-warn").remove();
+            $(".fc-marking").remove();
+            
+            if(!$.isNumeric(companyCap)){
+                canDo = 0;
+                var top = $("input[name=compCap]").offset().top - 68;
+                $(".founding-contract").prepend(`<div class="fc-marking" style="top: `+top+`px;"></div>`);
+                $(".fc-cont").after(`<div class="fc-warn">The Capital must be a number</div>`);
+            }
+
+            if($.isNumeric(companyCap) && companyCap > user.money){
+                canDo = 0;
+                var top = $("input[name=compCap]").offset().top - 68;
+                $(".founding-contract").prepend(`<div class="fc-marking" style="top: `+top+`px;"></div>`);
+                $(".fc-cont").after(`<div class="fc-warn">Insufficient Funds</div>`);
+            }
+
+            if($.trim(companyName) == ''){
+                canDo = 0;
+                var top = $("input[name=compName]").offset().top - 68;
+                $(".founding-contract").prepend(`<div class="fc-marking" style="top: `+top+`px;"></div>`);
+                $(".fc-cont").after(`<div class="fc-warn">You must enter a name for the Company</div>`);
+            }
+            
+            if(canDo){
+                $(this).remove();
+                $(".fc-bottom-sign-button-cancel").remove();
+                var signAngle = chance.integer({min: -10, max: 10});
+                var stampAngle = chance.integer({min: 0, max: 359});
+                $(".fc-bottom-signature").append(`
+<div class="fc-bs-signature username" style="transform: rotate(`+signAngle+`deg);">`+user.name+`</div>
+<div class="fc-bs-stamp" style="transform: rotate(`+stampAngle+`deg);"></div>
+`);
+                user.money -= companyCap;
+                user.createCompany(companyName, companyCap);
+                world.isPaused = 0;
+                refreshInfo();
+                setTimeout(function(){
+                    $(".modalOverlay").remove();
+                }, 2000);
+            }
+        });
+        
+        $(document).on("click", ".trs-addNewCompButton", function(){
+           
+            if(!$(".modalOverlay").length){
+                world.isPaused = 1;
+            $("body").prepend(`
+<div class="modalOverlay">
+<div class="founding-contract">
+<div class="founding-contract-title">Bill of Property</div>
+<div class="fc-topDetails">
+<div class="fc-topDetail">
+<div class="fc-td-left">Name</div>
+<div class="fc-td-right username">`+user.name+`</div>
+</div>
+<div class="fc-topDetail">
+<div class="fc-td-left">Company Name</div>
+<input class="fc-td-right username" type="text" name="compName">
+</div>
+<div class="fc-topDetail">
+<div class="fc-td-left">Capital</div>
+<label>$</label>
+<input class="fc-td-right username" type="number" name="compCap">
+</div>
+</div>
+<div class="fc-cont">
+Capicola pork shoulder drumstick, pork turducken tip kielbasa. Mignon ham ham 
+belly biltong, hamburger round ham tri-tip braunschweiger. Drumstick strip 
+leberkas landjaeger tip beef braunschweiger tenderloin pork sausage, pork 
+sirloin frankfurter ham turkey ham ham boudin brisket. Turducken jerky bacon ham 
+pork. Ribs turducken fatback, strip prosciutto turducken ham shankle ham chuck 
+andouille
+</div>
+<div class="fc-bottom">
+<div class="fc-bottom-date">
+<div class="fc-bottom-date-title">Date</div>
+<div class="fc-bottom-date-date worldFormatedDate">`+world.formatedDate+`</div>
+</div>
+
+<div class="fc-bottom-signature">
+<div class="fc-bottom-sign-title">Signature</div>
+<div class="fc-bottom-sign-button-cancel">Cancel</div>
+<div class="fc-bottom-sign-button">Sign here</div>
+</div>
+</div>
+</div>
+</div>
+`);
+            }
+        });
+        
+        $(document).on("click", ".fc-bottom-sign-button-cancel", function(){
+           $(".modalOverlay").remove();
+            world.isPaused = 0;
         });
 
     });
